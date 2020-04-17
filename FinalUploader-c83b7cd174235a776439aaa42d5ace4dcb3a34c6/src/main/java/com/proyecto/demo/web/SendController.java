68e6170a53b4faecd50c01d0007927cbecf51110
MRussellM <mrussello@ucenfotec.ac.cr>
2020-04-16 20:33:25 -0600

    Creación de index estructura visual

--- FinalUploader-c83b7cd174235a776439aaa42d5ace4dcb3a34c6/src/main/java/com/proyecto/demo/web/SendController.java
+++ FinalUploader-c83b7cd174235a776439aaa42d5ace4dcb3a34c6/src/main/java/com/proyecto/demo/web/SendController.java
@@ -1,12 +1,16 @@
 package com.proyecto.demo.web;
 
+import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.proyecto.demo.domain.User;
 
 @RestController
@@ -17,11 +21,13 @@ public class SendController {
 	@Autowired
     private JmsTemplate jmsTemplate;
 
+
+
+	
     @PostMapping("/messages")
     public String postMessage(@RequestParam String message) {
         
         jmsTemplate.convertAndSend(DESTINATION_NAME, new User(message).getFirstName());
         return message;
     }
-//gygyg
 }
--- FinalUploader-c83b7cd174235a776439aaa42d5ace4dcb3a34c6/src/main/java/com/proyecto/demo/web/UserController.java
+++ FinalUploader-c83b7cd174235a776439aaa42d5ace4dcb3a34c6/src/main/java/com/proyecto/demo/web/UserController.java
@@ -1,11 +1,14 @@
 package com.proyecto.demo.web;
 
+import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +32,15 @@ public class UserController {
 	@Autowired
     private JmsTemplate jmsTemplate;
 
+	@RequestMapping("")
+	public String indexPage(Model model) throws ParseException {
+		User user = new User();
+		model.addAttribute("user", new User());
+		model.addAttribute("user", user);
+		return "index";
+	}
+
+	
 	@PostMapping("/user")
 	public ResponseEntity<String> addUser(@RequestBody User newUser) {
 		jmsTemplate.convertAndSend(DESTINATION_NAME, newUser.getFirstName());
@@ -41,5 +53,4 @@ public class UserController {
 		
 		return serv.getAllUsers();
 	}
-	//uhu
 }
